0c555ba39d61ca98c603ab4e121b5cd7d3080436
Dexutar <hasimop7@gmail.com>
2020-06-21 12:52:03 +0200

    Fixed thread creation for EventLoops.

--- src/network/base/EventLoop.cpp
+++ src/network/base/EventLoop.cpp
@@ -3,7 +3,7 @@
 EventLoop::EventLoop (unsigned int thread_count) : work_guard(io_context.get_executor())
 {
   for (unsigned int i = 0; i < thread_count; ++i)
-    threads.emplace_back(&boost::asio::io_context::run, io_context);
+    threads.emplace_back([&](){io_context.run();});
 }
 
 void EventLoop::shutdown ()
