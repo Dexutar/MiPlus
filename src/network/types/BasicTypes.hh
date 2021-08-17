e5e7bd02ead35c29f1bc9252c0571dcf7b5edcdc
Dexutar <hasimop7@gmail.com>
2021-08-17 15:06:32 +0200

    64 bit int write and read implimented.

--- src/network/types/BasicTypes.hh
+++ src/network/types/BasicTypes.hh
@@ -17,10 +17,27 @@ public:
       return __builtin_bswap16(res);
   }
 
+  static std::uint64_t readUint64 (std::istream &is)
+  {
+    std::uint64_t res = 0;
+    is.read(reinterpret_cast<char*>(&res),sizeof(res));
+    if constexpr (std::endian::native == std::endian::big)
+      return res;
+    else
+      return __builtin_bswap64(res);
+  }
+
   static void writeUint16 (std::ostream &os, std::uint16_t value)
   {
     if constexpr (std::endian::native != std::endian::big)
       value = __builtin_bswap16(value);
+    os.write(reinterpret_cast<char*>(&value),sizeof(value));
+  }
+
+  static void writeUint64 (std::ostream &os, std::uint64_t value)
+  {
+    if constexpr (std::endian::native != std::endian::big)
+      value = __builtin_bswap64(value);
     os.write(reinterpret_cast<char*>(&value),sizeof(value));
   }
 };
