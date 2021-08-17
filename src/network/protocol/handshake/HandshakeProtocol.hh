38b0ef87ca1364978e420aadedf808f4b8b50cb3
Dexutar <hasimop7@gmail.com>
2021-08-17 14:21:49 +0200

    Add handshake protocol folder.

--- src/network/protocol/handshake/HandshakeProtocol.hh
+++ src/network/protocol/handshake/HandshakeProtocol.hh
@@ -8,14 +8,11 @@ class HandshakeProtocol : public Protocol
 {
 public:
 
-  HandshakeProtocol (Session &session) : Protocol{session}
-  {}
+  HandshakeProtocol (Session &session) : Protocol{session} {}
 
   void inbound (std::istream &is) override;
 
 private:
 
   void handle (const HandshakePacket &packet);
-
-  bool first = false;
 };
