b11b72276d32879933d1b658dc8493262d18d438
Dexutar <hasimop7@gmail.com>
2021-08-17 14:48:57 +0200

    Implemented status server response packet.

---
+++ src/network/packet/status/server/ResponsePacket.cpp
@@ -0,0 +1,16 @@
+#include "ResponsePacket.hh"
+
+#include <sstream>
+
+#include "VarString.hh"
+
+std::ostream& ResponsePacket::write (std::ostream &os) const
+{
+  std::stringbuf sb;
+  std::ostream data{&sb};
+  
+  VarNumber::writeVarNumber(data, ResponsePacket::opcode);
+  VarString::writeVarString(data, response);
+
+  return write_header(os, data);
+}
---
+++ src/network/packet/status/server/ResponsePacket.hh
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+
+#include "Packet.hh"
+
+class ResponsePacket : public Packet
+{
+public:
+  static const std::uint8_t opcode = 0;
+
+  ResponsePacket(std::string &&response) : response{response} {}
+
+private:
+  std::ostream& write (std::ostream &os) const override;
+
+  std::string response;
+}
