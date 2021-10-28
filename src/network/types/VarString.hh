6511cd33af0c27aa7f10d6e40c0c0c7f023af595
Dexutar <hasimop7@gmail.com>
2021-10-28 19:22:01 +0000

    Use VarNumber concepts in VarString

--- src/network/types/VarString.hh
+++ src/network/types/VarString.hh
@@ -2,10 +2,24 @@
 
 #include <string>
 
+#include "VarNumber.hh"
+
 class VarString
 {
  public:
-  static std::string readVarString(std::istream &is);
+  template <VarNumberReader VN = VarNumber>
+  static std::string readVarString(std::istream &is)
+  {
+    std::string res;
+    int size = VN::readVarInt(is);
+    std::copy_n(std::istream_iterator<char>(is), size, std::back_inserter(res));
+    return res;
+  }
 
-  static void writeVarString(std::ostream &os, const std::string &string);
+  template <VarNumberWriter<std::size_t> VN = VarNumber>
+  static void writeVarString(std::ostream &os, std::string_view string)
+  {
+    VN::writeVarNumber(os, string.size());
+    os << string;
+  }
 };
