2052453ee4e8091d4a305efc2d05ac74173849d3
Pau Garcia Rodriguez <hasimop7@gmail.com>
2021-10-31 10:06:52 +0000

    Move VarString method definitions

--- src/network/types/VarString.hh
+++ src/network/types/VarString.hh
@@ -25,18 +25,25 @@ class VarString
 {
  public:
   template <VarNumberReader VN = VarNumber>
-  static std::string readVarString(std::istream &is)
-  {
-    std::string res;
-    int size = VN::readVarInt(is);
-    std::copy_n(std::istream_iterator<char>(is), size, std::back_inserter(res));
-    return res;
-  }
+  static std::string readVarString(std::istream &is);
 
   template <VarNumberWriter<std::size_t> VN = VarNumber>
-  static void writeVarString(std::ostream &os, std::string_view string)
-  {
-    VN::writeVarNumber(os, string.size());
-    os << string;
-  }
+  static void writeVarString(std::ostream &os, std::string_view string);
 };
+
+
+template <VarNumberReader VN>
+std::string VarString::readVarString(std::istream &is)
+{
+  std::string res;
+  int size = VN::readVarInt(is);
+  std::copy_n(std::istream_iterator<char>(is), size, std::back_inserter(res));
+  return res;
+}
+
+template <VarNumberWriter<std::size_t> VN>
+void VarString::writeVarString(std::ostream &os, std::string_view string)
+{
+  VN::writeVarNumber(os, string.size());
+  os << string;
+}
