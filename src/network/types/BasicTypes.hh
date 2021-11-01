cc3786e3b82c5128f86153b196de1ffb6e993d1c
Pau Garcia Rodriguez <hasimop7@gmail.com>
2021-11-01 16:17:36 +0000

    Added BasicTypes concepts

--- src/network/types/BasicTypes.hh
+++ src/network/types/BasicTypes.hh
@@ -5,19 +5,36 @@
 #include <istream>
 #include <ostream>
 
+template <typename T>
+concept BasicTypesReader = requires(std::istream &is)
+{
+  {
+    T::template read<std::uint16_t>(is)
+    } -> std::same_as<std::uint16_t>;
+  {
+    T::template read<std::uint64_t>(is)
+    } -> std::same_as<std::uint64_t>;
+};
+
+template <typename T, typename number>
+concept BasicTypesWriter = std::integral<number> and requires(std::ostream &os, number value)
+{
+  {
+    T::write(os, value)
+    } -> std::same_as<void>;
+};
+
 class BasicTypes
 {
  public:
   template <std::integral number>
   static number read(std::istream &is);
 
-
   template <std::integral number>
   static void write(std::ostream &os, number value);
 };
 
-
-template<>
+template <>
 std::uint16_t BasicTypes::read(std::istream &is)
 {
   std::uint16_t res = 0;
@@ -28,7 +45,7 @@ std::uint16_t BasicTypes::read(std::istream &is)
     return __builtin_bswap16(res);
 }
 
-template<>
+template <>
 std::uint64_t BasicTypes::read(std::istream &is)
 {
   std::uint64_t res = 0;
@@ -39,14 +56,14 @@ std::uint64_t BasicTypes::read(std::istream &is)
     return __builtin_bswap64(res);
 }
 
-template<>
+template <>
 void BasicTypes::write(std::ostream &os, std::uint16_t value)
 {
   if constexpr (std::endian::native != std::endian::big) value = __builtin_bswap16(value);
   os.write(reinterpret_cast<char *>(&value), sizeof(value));
 }
 
-template<>
+template <>
 void BasicTypes::write(std::ostream &os, std::uint64_t value)
 {
   if constexpr (std::endian::native != std::endian::big) value = __builtin_bswap64(value);
