6f693ba7a643ee75d074938f243254730bae4b16
Pau Garcia Rodriguez <hasimop7@gmail.com>
2021-11-01 16:20:06 +0000

    Templatize HandshakePacket inputstream operator

--- src/network/packet/handshake/client/HandshakePacket.hh
+++ src/network/packet/handshake/client/HandshakePacket.hh
@@ -2,18 +2,29 @@
 
 #include <string>
 
+#include "BasicTypes.hh"
 #include "ConnectionState.hh"
 #include "Packet.hh"
+#include "VarNumber.hh"
+#include "VarString.hh"
 
 class HandshakePacket : Packet
 {
  public:
   static constexpr std::uint8_t opcode = 0;
 
-  friend std::istream &operator>>(std::istream &is, HandshakePacket &packet);
+  template <VarNumberReader VN = VarNumber, VarStringReader VS = VarString,
+            BasicTypesReader BR = BasicTypes>
+  friend std::istream &operator>>(std::istream &is, HandshakePacket &packet)
+  {
+    packet.version = VN::readVarInt(is);
+    packet.server_address = VS::readVarString(is);
+    packet.server_port = BR::template read<std::uint16_t>(is);
+    packet.requested_state = static_cast<ConnectionState>(VN::readVarInt(is));
+    return is;
+  }
 
   ConnectionState getRequestedState() const;
-  void print() const;
 
  private:
   std::ostream &write(std::ostream &os) const override;
