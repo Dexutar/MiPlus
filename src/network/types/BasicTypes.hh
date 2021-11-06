c68fc90cbfab2fae6cda99498211539fb587e6ca
Dexutar <hasimop7@gmail.com>
2021-11-06 17:01:40 +0000

    Fix BasicTypes definition templates

--- src/network/types/BasicTypes.hh
+++ src/network/types/BasicTypes.hh
@@ -3,6 +3,7 @@
 #include <bit>
 #include <concepts>
 #include <istream>
+#include <limits>
 #include <ostream>
 
 template <typename T>
@@ -28,16 +29,33 @@ class BasicTypes
 {
  public:
   template <std::integral number>
-  static number read(std::istream &is);
+  requires(std::numeric_limits<number>::digits + std::numeric_limits<number>::is_signed ==
+           16) static number read(std::istream &is);
 
   template <std::integral number>
-  static void write(std::ostream &os, number value);
+  requires(std::numeric_limits<number>::digits + std::numeric_limits<number>::is_signed ==
+           32) static number read(std::istream &is);
+
+  template <std::integral number>
+  requires(std::numeric_limits<number>::digits + std::numeric_limits<number>::is_signed ==
+           64) static number read(std::istream &is);
+
+  template <std::integral number>
+  requires(std::numeric_limits<number>::digits + std::numeric_limits<number>::is_signed ==
+           16) static void write(std::ostream &os, number value);
+  template <std::integral number>
+  requires(std::numeric_limits<number>::digits + std::numeric_limits<number>::is_signed ==
+           32) static void write(std::ostream &os, number value);
+  template <std::integral number>
+  requires(std::numeric_limits<number>::digits + std::numeric_limits<number>::is_signed ==
+           64) static void write(std::ostream &os, number value);
 };
 
-template <>
-std::uint16_t BasicTypes::read(std::istream &is)
+template <std::integral number>
+requires(std::numeric_limits<number>::digits + std::numeric_limits<number>::is_signed ==
+         16) number BasicTypes::read(std::istream &is)
 {
-  std::uint16_t res = 0;
+  number res = 0;
   is.read(reinterpret_cast<char *>(&res), sizeof(res));
   if constexpr (std::endian::native == std::endian::big)
     return res;
@@ -45,10 +63,23 @@ std::uint16_t BasicTypes::read(std::istream &is)
     return __builtin_bswap16(res);
 }
 
-template <>
-std::uint64_t BasicTypes::read(std::istream &is)
+template <std::integral number>
+requires(std::numeric_limits<number>::digits + std::numeric_limits<number>::is_signed ==
+         32) number BasicTypes::read(std::istream &is)
 {
-  std::uint64_t res = 0;
+  number res = 0;
+  is.read(reinterpret_cast<char *>(&res), sizeof(res));
+  if constexpr (std::endian::native == std::endian::big)
+    return res;
+  else
+    return __builtin_bswap32(res);
+}
+
+template <std::integral number>
+requires(std::numeric_limits<number>::digits + std::numeric_limits<number>::is_signed ==
+         64) number BasicTypes::read(std::istream &is)
+{
+  number res = 0;
   is.read(reinterpret_cast<char *>(&res), sizeof(res));
   if constexpr (std::endian::native == std::endian::big)
     return res;
@@ -56,15 +87,25 @@ std::uint64_t BasicTypes::read(std::istream &is)
     return __builtin_bswap64(res);
 }
 
-template <>
-void BasicTypes::write(std::ostream &os, std::uint16_t value)
+template <std::integral number>
+requires(std::numeric_limits<number>::digits + std::numeric_limits<number>::is_signed ==
+         16) void BasicTypes::write(std::ostream &os, number value)
 {
   if constexpr (std::endian::native != std::endian::big) value = __builtin_bswap16(value);
   os.write(reinterpret_cast<char *>(&value), sizeof(value));
 }
 
-template <>
-void BasicTypes::write(std::ostream &os, std::uint64_t value)
+template <std::integral number>
+requires(std::numeric_limits<number>::digits + std::numeric_limits<number>::is_signed ==
+         32) void BasicTypes::write(std::ostream &os, number value)
+{
+  if constexpr (std::endian::native != std::endian::big) value = __builtin_bswap32(value);
+  os.write(reinterpret_cast<char *>(&value), sizeof(value));
+}
+
+template <std::integral number>
+requires(std::numeric_limits<number>::digits + std::numeric_limits<number>::is_signed ==
+         64) void BasicTypes::write(std::ostream &os, number value)
 {
   if constexpr (std::endian::native != std::endian::big) value = __builtin_bswap64(value);
   os.write(reinterpret_cast<char *>(&value), sizeof(value));
